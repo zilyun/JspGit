b0cc83cc423a6852bf18ed80d8a3a8535bc546b0
DESKTOP-SO3G7VU\jhta <a66074742@gmail.com>
2024-03-25 16:03:40 +0900

    2. test_branch에서 Test.java를 작성합니다.

---
+++ JspGit/src/main/java/test/Test.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/index2")
+public class Test extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public Test() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			response.getWriter().print("Served at: " + request.getContextPath() + "Test");
+	}
+
+}
